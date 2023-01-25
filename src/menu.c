950dc5dbfea3fff4181df35b26904470c2e1516d
Noran Raskin <n.raskin@campus.tu-berlin.de>
2023-01-25 02:16:14 +0100

    Add menu entry to set max score

--- src/menu.c
+++ src/menu.c
@@ -18,11 +18,12 @@ void menu() {
     keypad(stdscr, TRUE);  // enable arrow keys
 
     // Show the menu
-    int highlight = 0;
+    int highlight = 3;
     int choice;
     GameMode gameMode = NORMAL;
     int chosenResolution = 1;
-    const char* options[] = {"Gamemode: %s", "Resolution: %s", "Start Game", "Exit"};
+    int gamepoints = 5;
+    const char* options[] = {"Gamemode: %s", "Points: %2d", "Resolution: %s", "Start Game", "Exit"};
     const char* gameModes[] = {"PVP   ", "Normal", "Hard  "};
     const char* resolutions[] = {" 640 x 480 ", " 800 x 600 ", "1200 x 900 ", "1400 x 1050"};
     const double resFactors[] = {0.8, 1.0, 1.5, 1.75};
@@ -36,19 +37,22 @@ void menu() {
         mvprintw(terminalHeight / 2 - n_options, terminalWidth / 2 - strlen("Pong") /2, "Pong");
         attroff(A_UNDERLINE | A_BLINK);
         for (int i = 0; i < n_options; i++) {
+            int Y = terminalHeight / 2 - n_options / 2 + i;
             if (i == highlight)
                 attron(A_STANDOUT);
             if (i == 0) {
-                mvprintw(terminalHeight / 2 - n_options / 2 + i, terminalWidth / 2 - (strlen(options[i])+5) / 2, options[i], gameModes[gameMode]);
+                mvprintw(Y, terminalWidth / 2 - (strlen(options[i]) + 5) / 2, options[i], gameModes[gameMode]);
             } else if (i == 1) {
-                mvprintw(terminalHeight / 2 - n_options / 2 + i, terminalWidth / 2 - (strlen(options[i]) + 8) / 2, options[i], resolutions[chosenResolution]);
+                mvprintw(Y, terminalWidth / 2 - (strlen(options[i]) + 1) / 2, options[i], gamepoints);
             } else if (i == 2) {
+                mvprintw(Y, terminalWidth / 2 - (strlen(options[i]) + 6 ) / 2, options[i], resolutions[chosenResolution]);
+            } else if (i == 3) {
                 attron(A_BOLD);
-                mvprintw(terminalHeight / 2 - n_options / 2 + i + 1, terminalWidth / 2 - strlen(options[i]) / 2, options[i]);
+                mvprintw(Y + 1, terminalWidth / 2 - strlen(options[i]) / 2, options[i]);
                 attroff(A_BOLD);
             } else {
                 attron(A_DIM);
-                mvprintw(terminalHeight / 2 - n_options / 2 + i + 3, terminalWidth / 2 - strlen(options[i]) / 2, options[i]);
+                mvprintw(Y + 3, terminalWidth / 2 - strlen(options[i]) / 2, options[i]);
                 attroff(A_DIM);
             }
             attroff(A_STANDOUT);
@@ -73,6 +77,11 @@ void menu() {
                     }
                 }
                 if (highlight == 1) {
+                    if (gamepoints > 1) {
+                        gamepoints--;
+                    }
+                }
+                if (highlight == 2) {
                     if (chosenResolution > 0) {
                         chosenResolution--;
                         scale_factor = resFactors[chosenResolution];
@@ -86,19 +95,23 @@ void menu() {
                     }
                 }
                 if (highlight == 1) {
+                    if (gamepoints < 99) {
+                        gamepoints++;
+                    }
+                }
+                if (highlight == 2) {
                     if (chosenResolution < 3) {
                         chosenResolution++;
                         scale_factor = resFactors[chosenResolution];
                     }
                 }
                 break;
-            case KEY_ENTER:
             case ' ':
-                if (highlight == 2) {
-                    game(gameMode, 5);
+                if (highlight == 3) {
+                    game(gameMode, gamepoints);
                     break;
                 }
-                else if (highlight == 3){
+                else if (highlight == 4){
                     endwin();
                     return;
                 }
